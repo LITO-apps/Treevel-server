24ac3b334b7147a089e92f4bf0b5f085147bb8c7
xiii111 <aoi.t19950517@gmail.com>
2020-03-29 18:41:06 +0900

    Organize create statements

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,5 +1,16 @@
 use db;
 
+CREATE TABLE IF NOT EXISTS player
+(
+    id                INT NOT NULL AUTO_INCREMENT,
+    name              VARCHAR(64) NOT NULL COMMENT 'プレイヤーネーム',
+    playing_stage_id  INT COMMENT 'プレイ中のステージID',
+    create_time       TIMESTAMP NOT NULL COMMENT 'アカウント作成時刻',
+    last_login_time   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '最終ログイン時刻',
+
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS record
 (
     id                INT NOT NULL AUTO_INCREMENT,
@@ -9,5 +20,6 @@ CREATE TABLE IF NOT EXISTS record
     play_times        INT NOT NULL COMMENT 'プレイ回数',
     first_clear_times INT COMMENT '初回クリアまでのプレイ回数',
     min_clear_time    TIME COMMENT '最短クリア時間',
+
     PRIMARY KEY (id)
 );
