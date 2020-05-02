537b60c4ef7a8acab0b5af59140dd9b00ccbe7f5
xiii111 <aoi.t19950517@gmail.com>
2020-05-02 16:18:46 +0900

    Fix for review

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/LITO-apps/Treevel-server
 
 go 1.13
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -1,38 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/httputil"
+    "fmt"
+    "log"
+    "net/http"
+    "net/http/httputil"
 
-	"github.com/julienschmidt/httprouter"
+    "github.com/julienschmidt/httprouter"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request, pr httprouter.Params)  {
-	dump, err := httputil.DumpRequest(r, true)
+func rootHandler(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+    dump, err := httputil.DumpRequest(r, true)
 
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
 
-	fmt.Println(string(dump))
+    fmt.Println(string(dump))
 
-	_, err = fmt.Fprint(w, "hello world!\n")
+    _, err = fmt.Fprint(w, "hello world!\n")
 
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
 }
 
 func main()  {
-	// ルーティングの設定
-	router := httprouter.New()
-	router.GET("/", rootHandler)
+    // ルーティングの設定
+    router := httprouter.New()
+    router.GET("/", rootHandler)
 
-	// サーバ起動
-	fmt.Println("Server Start")
-	log.Fatal(http.ListenAndServe(":8080", router))
+    err := http.ListenAndServe(":8080", router)
+    if err != nil {
+        log.Fatalf("Listen and serve failed. %+v", err)
+    } else {
+        // サーバ起動
+        fmt.Println("Server Start")
+    }
 }
