424809d4cff4c6e37b57608056c7f7561e31eb57
xiii111 <aoi.t19950517@gmail.com>
2020-05-23 03:31:30 +0900

    Implement url to get all players and records

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
     "fmt"
+    "github.com/LITO-apps/Treevel-server/models"
     "log"
     "net/http"
     "net/http/httputil"
 
+    "github.com/gobuffalo/pop"
     "github.com/julienschmidt/httprouter"
 )
 
@@ -27,10 +29,74 @@ func rootHandler(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
     }
 }
 
+func getAllPlayersHandler(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+    dump, err := httputil.DumpRequest(r, true)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println(string(dump))
+
+    tx, err := pop.Connect("development")
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    var players []models.Player
+    err = tx.All(&players)
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    _, err = fmt.Fprint(w, players)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+}
+
+func getAllRecordsHandler(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+    dump, err := httputil.DumpRequest(r, true)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println(string(dump))
+
+    tx, err := pop.Connect("development")
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    var records []models.Record
+    err = tx.All(&records)
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    _, err = fmt.Fprint(w, records)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+}
+
 func main()  {
     // ルーティングの設定
     router := httprouter.New()
     router.GET("/", rootHandler)
+    router.GET("/get_all_players", getAllPlayersHandler)
+    router.GET("/get_all_records", getAllRecordsHandler)
 
     // サーバ起動
     fmt.Println("Server Start")
