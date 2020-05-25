8a47ca03fc865f9e2f1d4362f59b9608c9945813
takesi0627 <woody70825@gmail.com>
2020-05-25 23:12:32 +0900

    implement api of creating db records

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
     "log"
     "net/http"
     "net/http/httputil"
+    "strconv"
+    "time"
 
     "github.com/LITO-apps/Treevel-server/models"
+    "github.com/gobuffalo/nulls"
     "github.com/gobuffalo/pop"
     "github.com/julienschmidt/httprouter"
 )
@@ -91,12 +94,86 @@ func getAllRecordsHandler(w http.ResponseWriter, r *http.Request, pr httprouter.
     }
 }
 
+func createPlayer(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+    dump, err := httputil.DumpRequest(r, true)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println(string(dump))
+
+    // parse post data
+    name := r.FormValue("name")
+
+    t := time.Now()
+
+    db, err := pop.Connect("development")
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    player := models.Player{Name: name, LastLoginTime: t}
+    _, err = db.ValidateAndCreate(&player)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+}
+
+func createRecord(w http.ResponseWriter, r *http.Request, pr httprouter.Params) {
+    dump, err := httputil.DumpRequest(r, true)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println(string(dump))
+
+    // parse post data
+    playerID, err := strconv.Atoi(r.FormValue("player_id"))
+    stageID, err := strconv.Atoi(r.FormValue("stage_id"))
+    isClear, err := strconv.ParseBool(r.FormValue("is_clear"))
+    playTimes, err := strconv.Atoi(r.FormValue("play_times"))
+    firstClearTimes, err := strconv.Atoi(r.FormValue("first_clear_times"))
+    minClearTime := r.FormValue("min_clear_time")
+
+    db, err := pop.Connect("development")
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+
+    record := models.Record {
+        PlayerID: playerID, 
+        StageId: stageID,
+        IsClear: isClear,
+        PlayTimes: playTimes,
+        FirstClearTimes: nulls.NewInt(firstClearTimes),
+        MinClearTime: nulls.NewString(minClearTime),
+    }
+    _, err = db.ValidateAndSave(&record)
+
+    if err != nil {
+        http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+        return
+    }
+}
+
+
+
 func main() {
     // ルーティングの設定
     router := httprouter.New()
     router.GET("/", rootHandler)
     router.GET("/get_all_players", getAllPlayersHandler)
     router.GET("/get_all_records", getAllRecordsHandler)
+    router.POST("/create_player", createPlayer)
+    router.POST("/create_record", createRecord)
 
     // サーバ起動
     fmt.Println("Server Start")
--- models/player.go
+++ models/player.go
@@ -10,6 +10,6 @@ type Player struct {
     ID              int         `db:"id"`
     Name            string      `db:"name"`
     PlayingStageID  nulls.Int   `db:"playing_stage_id"`
-    CreateTime      time.Time   `db:"create_time"`
+    CreatedAt       time.Time   `db:"create_time"`
     LastLoginTime   time.Time   `db:"last_login_time"`
 }
