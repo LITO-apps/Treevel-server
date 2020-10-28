dea3525e28b33c2a01015024a2af760340bf4986
Takeshi <woody70825@gmail.com>
2020-10-28 23:35:56 +0900

    use upper letter for keyword
    Co-authored-by: Aoi Takahashi <aoi.t19950517@gmail.com>

--- migrations/20201024121554_alter_record_clear_time.mysql.up.sql
+++ migrations/20201024121554_alter_record_clear_time.mysql.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE records DROP COLUMN min_clear_time;
-ALTER TABLE records ADD COLUMN clear_time float COMMENT '最短クリア時間';
+ALTER TABLE records ADD COLUMN clear_time FLOAT COMMENT '最短クリア時間';
